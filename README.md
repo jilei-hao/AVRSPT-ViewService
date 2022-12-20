3b82ed6b4c31346e4001f1ccd051cc9ae761a078
jilei-hao <jilei.cs@gmail.com>
2022-12-20 14:13:41 -0500

    barely working

---
+++ README.md
@@ -0,0 +1,11 @@
+### Dependencies
+react v16.14.0
+react-dom v16.14.0
+react-router-dom v5.3.4
+react-vtk-js v1.15.4
+@kitware/vtk.js 25.13.1
+
+### Notes
+- We use react v16 instead of v18 is because react-vtk-js is not working well with v18.
+- To work with react v16, we have to use react-router-dom v5.3.4
+  - Docs: https://v5.reactrouter.com/web/guides/quick-start
--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,77 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
+import ReactDOM from 'react-dom'
 
 import {
-  createBrowserRouter,
-  RouterProvider,
+  BrowserRouter as Router,
+  Switch,
   Route,
+  Link
 } from "react-router-dom";
 
 /* Routes files import */
-import Root from "./routes/root";
+import Root from "./routes/root.jsx";
 import Model from "./routes/model/model";
 import Slices from "./routes/slices/slices";
 import Volume from "./routes/volume/volume";
 import Sandbox from "./routes/sandbox/sandbox";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-  },
-  {
-    path: "/Model",
-    element: <Model />,
-  },
-  {
-    path: "/Slices",
-    element: <Slices />,
-  },
-  {
-    path: "/Volume",
-    element: <Volume />,
-  },
-  {
-    path: "/Sandbox",
-    element: <Sandbox />,
-  }
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Root />,
+//   },
+//   {
+//     path: "/Model",
+//     element: <Model />,
+//   },
+//   {
+//     path: "/Slices",
+//     element: <Slices />,
+//   },
+//   {
+//     path: "/Volume",
+//     element: <Volume />,
+//   },
+//   {
+//     path: "/Sandbox",
+//     element: <Sandbox />,
+//   }
+// ]);
+export default function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/users">Users</Link>
+            </li>
+          </ul>
+        </nav>
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-)
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/model">
+            <Model />
+          </Route>
+          <Route path="/slices">
+            <Slices />
+          </Route>
+          <Route path="/volume">
+            <Volume />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
