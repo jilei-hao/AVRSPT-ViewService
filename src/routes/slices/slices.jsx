42a55a1df3955cf66e61f40941363386548312f2
jilei-hao <jilei.cs@gmail.com>
2022-12-21 13:58:28 -0500

    experiment stop point

--- src/routes/slices/slices.jsx
+++ src/routes/slices/slices.jsx
@@ -1,177 +1,172 @@
-import { useState, useRef, useEffect } from 'react';
+import React, { useState, useContext, useEffect, useRef } from 'react';
+import vtkColorMaps from '@kitware/vtk.js/Rendering/Core/ColorTransferFunction/ColorMaps.js';
 
-// Load the rendering pieces we want to use (for both WebGL and WebGPU)
-import '@kitware/vtk.js/Rendering/Profiles/Geometry';
+import {
+  View,
+  ShareDataSet,
+  SliceRepresentation,
+  Reader,
+  Contexts,
+  VolumeController,
+  VolumeRepresentation,
+} from 'react-vtk-js';
 
-// Force DataAccessHelper to have access to various data source
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
-import '@kitware/vtk.js/IO/Core/DataAccessHelper/HttpDataAccessHelper';
+function Slider(props) {
+  const view = useContext(Contexts.ViewContext);
+  const onChange = (e) => {
+    const value = Number(e.currentTarget.value);
+    props.setValue(value);
+    setTimeout(view.renderView, 0);
+  };
+  return (
+    <label
+      style={{
+        position: 'absolute',
+        zIndex: 100,
+        left: '5px',
+        top: '0px',
+        ...props.style,
+      }}
+    >
+      {props.label}
+      <input
+        type='range'
+        min='0'
+        max={props.max}
+        value={props.value}
+        onChange={onChange}
+        style={{
+          position: 'sticky',
+          zIndex: 100,
+          left: '5px',
+          top: '5px',
+          ...props.style,
+        }}
+      />
+    </label>
+  );
+}
 
-import vtkFullScreenRenderWindow from '@kitware/vtk.js/Rendering/Misc/FullScreenRenderWindow';
-import vtkActor           from '@kitware/vtk.js/Rendering/Core/Actor';
-import vtkMapper          from '@kitware/vtk.js/Rendering/Core/Mapper';
-import vtkHttpDataSetReader from '@kitware/vtk.js/IO/Core/HttpDataSetReader';
-import vtkImageMapper from '@kitware/vtk.js/Rendering/Core/ImageMapper';
-import vtkImageSlice from '@kitware/vtk.js/Rendering/Core/ImageSlice';
-import vtkConeSource from '@kitware/vtk.js/Filters/Sources/ConeSource';
+function DropDown(props) {
+  const view = useContext(Contexts.ViewContext);
+  function onChange(e) {
+    const value = e.currentTarget.value;
+    props.setValue(value);
+    setTimeout(view.renderView, 0);
+  }
+  return (
+    <select
+      value={props.value}
+      onChange={onChange}
+      style={{
+        position: 'absolute',
+        zIndex: 100,
+        left: '5px',
+        top: '5px',
+        ...props.style,
+      }}
+    >
+      {props.options.map((opt) => (
+        <option value={opt}>{opt}</option>
+      ))}
+    </select>
+  );
+}
 
-import styles from '../../app.module.css'
+function CheckBox(props) {
+  const view = useContext(Contexts.ViewContext);
+  function onChange(e) {
+    const value = e.currentTarget.checked;
+    props.setValue(value);
+    setTimeout(view.renderView, 0);
+  }
+  return (
+    <label
+      style={{
+        position: 'absolute',
+        zIndex: 100,
+        left: '5px',
+        top: '55px',
+        ...props.style,
+      }}
+    >
+      {props.label}
+      <input
+        type='checkbox'
+        checked={props.value}
+        onChange={onChange}
+        style={{
+          position: 'sticky',
+          zIndex: 100,
+          left: '105px',
+          ...props.style,
+        }}
+      />
+    </label>
+  );
+}
 
-export default function Slices() {
-  const vtkContainerRef = useRef(null);
-  const context = useRef(null); // vtk related objects
-  const [hasDataDownloaded, setHasDataDownloaded] = useState(false);
+function Slices(props) {
+  const [iSlice, setISlice] = useState(128);
+  const [jSlice, setJSlice] = useState(128);
+  const [kSlice, setKSlice] = useState(47);
+  const [colorWindow, setColorWindow] = useState(2095);
+  const [colorLevel, setColorLevel] = useState(1000);
+  const [colorPreset, setColorPreset] = useState('Grayscale');
+  const [useLookupTableScalarRange, setUseLookupTableScalarRange] =
+    useState(false);
 
-  const BASE_URL = 'https://kitware.github.io/vtk-js/data/volume/'
+  const view0 = useRef(null);
+  const data = useRef(null);
 
-  /* Initialize renderWindow, renderer, mapper and actor */
   useEffect(() => {
-    if (!context.current) {
-      console.log("rebuilding context...", context.current);
+    console.log("view0", view0.current.props);
+  }, [view0]);
 
-      const fullScreenRenderWindow = vtkFullScreenRenderWindow.newInstance({
-        rootContainer: vtkContainerRef.current, // html element containing this window
-      });
-
-      const renderWindow = fullScreenRenderWindow.getRenderWindow();
-      const renderer = fullScreenRenderWindow.getRenderer();
-
-      const imageActorI = vtkImageSlice.newInstance();
-      const imageActorJ = vtkImageSlice.newInstance();
-      const imageActorK = vtkImageSlice.newInstance();
-
-      // const testActor = vtkActor.newInstance();
-      // renderer.addActor(testActor);
-      // const coneSource = vtkConeSource.newInstance({
-      //   center: [0, 0, 0],
-      //   height: 50.0,
-      // });
-      // const mapper = vtkMapper.newInstance();
-      // mapper.setInputData(coneSource);
-      // testActor.setMapper(mapper);
-
-      renderer.addActor(imageActorK);
-      renderer.addActor(imageActorJ);
-      renderer.addActor(imageActorI);
-      
-      const imageMapperI = vtkImageMapper.newInstance();
-      const imageMapperJ = vtkImageMapper.newInstance();
-      const imageMapperK = vtkImageMapper.newInstance();
-
-      console.log("imageActorI(outside) isDeleted: ", imageActorI.isDeleted());
-
-
-      if (!hasDataDownloaded) {
-        const reader = vtkHttpDataSetReader.newInstance({
-          fetchGzip: true,
-        });
-        reader.setUrl(`${BASE_URL}/headsq.vti`, { loadData: true })
-          .then(() => {
-            if (!imageActorI || imageActorI.isDeleted())
-            {
-              console.log("Page re-rendered, abandoning data loading");
-              return;
-            }
-
-            const data = reader.getOutputData();
-            const dataRange = data.getPointData().getScalars().getRange();
-            const extent = data.getExtent();
-
-            console.log("Data Retrieved: ", data);
-            console.log("dataRange: ", dataRange);
-            console.log("extent:", extent);
-            
-            imageMapperI.setInputData(data);
-            imageMapperI.setISlice(30);
-            imageActorI.setMapper(imageMapperI);
-            console.log("slice I set");
-
-            imageMapperJ.setInputData(data);
-            imageMapperJ.setJSlice(30);
-            imageActorJ.setMapper(imageMapperJ);
-            console.log("slice J set");
-
-            imageMapperK.setInputData(data);
-            imageMapperK.setKSlice(30);
-            imageActorK.setMapper(imageMapperK);
-            console.log("slice K set");
-
-            // Set Color Window and Level
-            updateColorLevel((Range[1] + Range[2]) / 2);
-            updateColorWindow((Range[1]));
-
-            renderer.resetCamera();
-            renderer.resetCameraClippingRange();
-
-            renderWindow.render();
-
-            console.log("render called");
-            setHasDataDownloaded(true);
-          }
-        );
-      }
-
-      context.current = {
-        fullScreenRenderWindow,
-        renderWindow,
-        renderer,
-        imageActorI,
-        imageActorJ,
-        imageActorK,
-        imageMapperI,
-        imageMapperJ,
-        imageMapperK
-      };
-
-      window.vtkContext = context;
-    }
-
-    return () => {
-      if (context.current) {
-        console.log("container ref cleaning up...", context.current)
-        const { fullScreenRenderWindow, imageActorI, imageActorJ, imageActorK, 
-          imageMapperI, imageMapperJ, imageMapperK } = context.current;
-        imageActorI.delete();
-        imageActorJ.delete();
-        imageActorK.delete();
-        imageMapperI.delete();
-        imageMapperJ.delete();
-        imageMapperK.delete();
-        fullScreenRenderWindow.delete();
-        context.current = null;
-      }
-    };
-  }, [vtkContainerRef]);
-
-  function updateColorLevel(level) {
-    if (context.current) {
-      const {imageActorI, imageActorJ, imageActorK, renderWindow} = context.current;
-      imageActorI.getProperty().setColorLevel(level);
-      imageActorJ.getProperty().setColorLevel(level);
-      imageActorK.getProperty().setColorLevel(level);
-
-      renderWindow.render();
-    }
-  }
-  
-  function updateColorWindow(window) {
-    if (context.current) {
-      const {imageActorI, imageActorJ, imageActorK, renderWindow} = context.current;
-      imageActorI.getProperty().setColorWindow(window);
-      imageActorJ.getProperty().setColorWindow(window);
-      imageActorK.getProperty().setColorWindow(window);
-
-      renderWindow.render();
-    }
-  }
+  useEffect(() => {
+    console.log("data", data.current.props);
+  }, [data]);
 
   return (
-    <div>
-      <div ref={vtkContainerRef} />
-      <div className={styles.control_panel}>
-        <button>Button</button>
+    <div style={{ width: '100%', height: '100%' }}>
+      <div style={{ width: '100vw', height: '100vh', display: 'inline-block' }}>
+        <View
+          ref={view0}
+          id='0'
+          cameraPosition={[1, 0, 0]}
+          cameraViewUp={[0, 0, -1]}
+          cameraParallelProjection={false}
+          background={[0.3, 0.3, 0.3]}
+          showOrientationAxes={true}
+        >
+          <ShareDataSet ref={data}>
+            <Reader
+              vtkClass='vtkXMLImageDataReader'
+              url='http://10.102.180.67:8000/dist/img3d_bavcta008_baseline_00.vti'
+            />
+          </ShareDataSet>
+          <Slider
+            label='iSlice'
+            max={256}
+            value={iSlice}
+            setValue={setISlice}
+            style={{ position: 'relative', left: '5px' }}
+          />
+          <SliceRepresentation
+            iSlice={iSlice}
+            property={{
+              colorWindow,
+              colorLevel,
+              useLookupTableScalarRange,
+            }}
+            colorMapPreset={colorPreset}
+          >
+            <ShareDataSet />
+          </SliceRepresentation>
+        </View>
       </div>
     </div>
   );
 }
+
+export default Slices;
