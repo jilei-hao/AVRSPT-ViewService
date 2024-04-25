78261fb0faece2479ad3e1ad005657367ff78fcd
jilei-hao <jilei.cs@gmail.com>
2024-04-25 15:07:58 -0400

    fixed find study bug

--- src/applications/avrp_viewer/study_browser_page/study_preview_panel/index.jsx
+++ src/applications/avrp_viewer/study_browser_page/study_preview_panel/index.jsx
@@ -1,11 +1,29 @@
 import React, { useState } from 'react';
-import styles from './styles.module.css'
+import styles from './styles.module.css';
+import { useAVRPGlobal } from '../../avrp_global_context';
 
-export default function StudyPreviewPanel ({ studyData }) {
+export default function StudyPreviewPanel () {
+  const { caseStudyHeaders, studyId } = useAVRPGlobal();
+
+  console.log("[StudyPreviewPanel] studyId: ", studyId);
+
+  let selectedStudy = null;
+
+  for (let case_data of caseStudyHeaders) {
+    const find = case_data.studies.find(study => study.id == studyId);
+    if (find) {
+      selectedStudy = find;
+      break;
+    }
+  }
+
+  console.log("[StudyPreviewPanel] selectedStudy: ", selectedStudy);
+
+
   return (
     <div className={styles.panelContainer}>
       <div className={ styles.panelHeader }>
-        <span></span>
+        <span>{selectedStudy ? selectedStudy.study_name : ''}</span>
       </div>
     </div>
   );
